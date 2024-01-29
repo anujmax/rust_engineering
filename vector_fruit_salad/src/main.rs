3ffdc923e5cd7bc69028e82ece1e1f378a028a80
Anuj Agrawal <anujagrawal@192.168.1.8>
2024-01-29 09:11:38 +0530

    use of vector data structure

---
+++ vector_fruit_salad/src/main.rs
@@ -0,0 +1,16 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+fn main() {
+    let mut fruit = vec!["Apple", "Banana", "Pear", "Peach", "Strawberry"];
+    let mut rng = thread_rng();
+    fruit.shuffle(&mut rng);
+    println!("Fruit salad: ");
+    for (i, item) in fruit.iter().enumerate() {
+        if i != fruit.len() - 1 {
+            print!("{}, ", item);
+        } else {
+            println!("{}.", item);
+        }
+    }
+}
